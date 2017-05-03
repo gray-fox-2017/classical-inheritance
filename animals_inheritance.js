83b608f1d4675089841548142a1793c3e0df1264
stedyyulius <stedyyulius@gmail.com>
2017-05-03 11:30:36 +0700

    rapihin

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -2,24 +2,29 @@
 class Animals{
   constructor(components){
     this.Name = ""
-      if(components.hasOwnProperty("Name")){
-        this.Name = components ["Name"]}
+    if (components.hasOwnProperty("Name")) {
+      this.Name = components ["Name"]
+    }
 
     this.Health = 0
-      if(components.hasOwnProperty("Health")){
-        this.Health = components ["Health"]}
+    if (components.hasOwnProperty("Health")) {
+      this.Health = components ["Health"]
+    }
 
     this.Atk = 0
-      if(components.hasOwnProperty("Atk")){
-        this.Atk = components ["Atk"]}
+    if (components.hasOwnProperty("Atk")) {
+      this.Atk = components ["Atk"]
+    }
 
     this.Def = 0;
-      if(components.hasOwnProperty("Def")){
-        this.Def = components ["Def"]}
+    if (components.hasOwnProperty("Def")) {
+      this.Def = components ["Def"]
+    }
 
     this.Spd = 2;
-      if(components.hasOwnProperty("Spd")){
-        this.Spd = components ["Spd"]}
+    if (components.hasOwnProperty("Spd")) {
+      this.Spd = components ["Spd"]
+    }
 
 
     // this.num_leg = 2;
@@ -30,66 +35,66 @@ class Animals{
 
 
 class SuperPower{
-  constructor(){
+  constructor() {
   }
 
-eat(){
-  console.log(`Health + 20 / turn for 2 turns`)
-}
-
-Might(){
-  console.log (`ATK + 50%`)
+  eat() {
+    console.log(`Health + 20 / turn for 2 turns`)
   }
-}
 
-class dog extends Animals{
-    constructor(components){
-      super(components)
-    }
-
-bite(){
-  this.superpower = new SuperPower();
-  console.log("DOG USE BITE!! deal 40 damage")
+  might() {
+    console.log (`ATK + 50%`)
   }
 }
 
-class bat extends Animals {
+class Dog extends Animals{
   constructor(components){
     super(components)
-
+    this.superpower = new SuperPower();
   }
 
-agility(){
-  this.superpower = new SuperPower()
-  console.log("BAT USE AGILITY!! all ally animals gain +20 Speed for 2 turns")
+  bite(){
+    console.log("DOG USE BITE!! deal 40 damage")
   }
 }
 
-class frog extends Animals {
+class Bat extends Animals {
   constructor(components){
     super(components)
+    this.superpower = new SuperPower()
+
   }
 
-poison(){
-  this.superpower = new SuperPower()
-  console.log("FROG USE POISON!! all enemy animals are poisoned -10 hp / turn")
+  agility(){
+    console.log("BAT USE AGILITY!! all ally animals gain +20 Speed for 2 turns")
   }
 }
 
-class elephant extends Animals {
+class Frog extends Animals {
   constructor(components){
     super(components)
+    this.superpower = new SuperPower()
   }
 
-wall(){
-  this.superpower = new SuperPower();
-  console.log("Elephant took a defensive state! all enemies attacks are blocked for 1 turn")
+  poison(){
+    console.log("FROG USE POISON!! all enemy animals are poisoned -10 hp / turn")
   }
 }
-var Dog = new dog({Name: "Dog", Health: 70, Atk: 25, Def: 30, Spd: 4});
-var Bat = new bat({Name: "Bat", Health: 45, Atk: 5, Def: 10, Spd: 100 });
-var Frog = new frog({Name: "Frog", Health: 60, Atk: 15, Def: 5, Spd: 60 });
-var Elephant = new elephant({Name: "Elephant", Health: 150, Atk: 5, Def: 80, Spd: 5 });
+
+class Elephant extends Animals {
+  constructor(components){
+    super(components)
+    this.superpower = new SuperPower();
+  }
+
+  wall(){
+    console.log("Elephant took a defensive state! all enemies attacks are blocked for 1 turn")
+  }
+}
+var dog = new Dog({Name: "Dog", Health: 70, Atk: 25, Def: 30, Spd: 4});
+var bat = new Bat({Name: "Bat", Health: 45, Atk: 5, Def: 10, Spd: 100 });
+var frog = new Frog({Name: "Frog", Health: 60, Atk: 15, Def: 5, Spd: 60 });
+var elephant = new Elephant({Name: "Elephant", Health: 150, Atk: 5, Def: 80, Spd: 5 });
 
 console.log(Dog)
 Dog.bite()
@@ -104,4 +109,4 @@ console.log(Elephant)
 Elephant.wall()
 
 Elephant.superpower.eat();
-Dog.superpower.Might()
+Dog.superpower.might()
